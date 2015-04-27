1b90b9762c81e914f99ebc4a00feb27be7bb754a
Jeremy <j.thulliez@gmail.com>
2015-04-27 21:43:56 +0200

    Commit etude

---
+++ etude.php
@@ -0,0 +1,82 @@
+Chaque classe peut faire le tank, le DPS, le heal.
+Liste des différentes classes:
+  - Guerrier
+  - Mage
+  - Rodeur
+  - Moine
+  
+Ce qui veut dire qu'un Guerrier peut être le Heal du groupe comme le Tank, idem pour le Moine, le Mage et le Rodeur.
+Ce qui différencie une classe d'une autre, ce sont les compétences. Par exemple un Moine aura plus de compétence de 
+soin qu'un Rodeur. Et inversement un Rodeur aura plus de compétence de DPS qu'un Moine. Mais il n'empêche qu'un Moine
+peut être utilisé comme DPS dans un groupe.
+Chaque classe peut porter les différents type d'armure / arme.
+Les types d'armures sont:
+  - Tissu
+  - Cuir
+  - Maille
+  - Plate
+Le type d'armure avantage certaine caractéristique:
+  - Le niveau de défense des armures : Plate > Maille > Cuir > Tissu
+  - Le niveau de dextérité des armures : Tissu > Cuir > Maille > Plate
+  
+Les types d'armes sont:
+  - Epée -- 1 main
+  - Lance -- 2 mains
+  - Dague -- 1 main
+  - Bâton -- 2 mains
+  - Bouclier -- 1 main
+  - Marteau -- 1 main
+  - Espadon -- 2 mains
+  - Arc court -- 2 mains
+  - Arc long -- 2 mains
+  - Livre -- 2 mains
+  - Baguette -- 1 main
+  - Focus -- 1 main
+  
+Les races jouables sont:
+  - Humain
+      Aspect : 
+        Etre vivant d'apect humaine standard.
+      Description :
+        Premier peuple à fouler les étendues de Pangline
+  - Grem's
+      Aspect :
+        Etre vivant de petite taille, doué d'une forte intelligence. Il mesure 1/3 de la taille d'un humain 
+      Description : 
+        Ce peuple est apparut bien des siècles après celui des humains, comment ? on ne le sait toujours pas.
+        Il s'agit du secret le mieux gardé de leur civilisation.
+  - Touchar
+      Aspect :
+        Etre vivant de taille normale, ne supportant pas les rayons du soleil. La couleur de peau est de tendance
+        pâle.
+      Description :
+        Ce peuple étrange et sauvage, possède un grand ennemi... Le soleil, en effet leurs cellules se désintègrent
+        aux lueur du soleil. Pour survivre ils sont obligés de vivre la nuit.
+        
+La race non jouable est Holik.
+    Aspect : 
+      Sombre, de toutes corpulance. Démoniaque.
+    Description : 
+      Le premier être est issu de la sorcelerie, malheureusement il a reussi à appliquer se dupliquer grâce à la magie
+      et à la naiveté de son créateur. Et depuis ce moment il ne cesse de créer d'autres créatures, toutes aussi
+      immonde les unes que les autres.
+Chaque personnage possède un équipement; celui-ci est constitué de la manière suivante.
+  # Armure
+  - Tête
+  - Epaule
+  - Torse
+  - Jambe
+  - Botte
+  - Gant
+  # Arme
+  - Main droite
+  - Main gauche
+  # Bijoux
+  - Oreille droite
+  - Oreille gauche
+  - Cou
+  - Bague
+  - Bague
+En plus de son équipement, le personnage possède un inventaire. Il permet de stocker tous les items qui peut ramasser.
+Il est composé d'un nombre limité de place.
+Il n'y a pas de notion de mana. Il y a uniquement une barre de vie et d'expérience. Chaque compétence
