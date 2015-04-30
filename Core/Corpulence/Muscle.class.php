42a1b4ebd9fc7f31c155ddddd59751e970a2027e
JeremZed <j.thulliez@gmail.com>
2015-04-30 11:45:28 +0200

    Create Muscle.class.php

---
+++ Core/Corpulence/Muscle.class.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core\Corpulence;
+
+use Core\Corpulence;
+
+class Muscle extends Corpulence
+{
+	public function __construct()
+	{
+		$this->setLibelle('Corpulence musclée.');
+	}
+}
