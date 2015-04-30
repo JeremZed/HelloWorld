a54dd582aeb7a3543efa1f6a831cd9f479883633
JeremZed <j.thulliez@gmail.com>
2015-04-30 11:24:00 +0200

    Update Couleur.class.php

--- Core/Couleur.class.php
+++ Core/Couleur.class.php
@@ -4,7 +4,15 @@ namespace Core;
 
 abstract class Couleur
 {
-
-
-
+	protected $libelle;
+	
+	public function getLibelle()
+	{
+		return $this->libelle;
+	}
+	
+	public function setLibelle($libelle)
+	{
+		$this->libelle = $libelle;
+	}
 }
