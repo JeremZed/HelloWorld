506e1bfa0bcde1d1620d59c86f0c595c366b9b82
JeremZed <j.thulliez@gmail.com>
2015-04-30 11:42:21 +0200

    Create Aucune.class.php

---
+++ Core/Pilosite/Aucune.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Core\Pilosite;
+
+use Core\Pilosite;
+
+class Aucune extends Pilosite
+{
+	public function __construct()
+	{
+		$this->setLibelle('Cet personne est imberbe!!');
+	}
+}
