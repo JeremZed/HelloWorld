f2574b4c5e21710ef060897464f2c18fb530e66e
JeremZed <j.thulliez@gmail.com>
2015-04-30 11:54:48 +0200

    Update Apparence.class.php

--- Core/Apparence.class.php
+++ Core/Apparence.class.php
@@ -1,1 +1,53 @@
+<?php
 
+namespace Core;
+
+abstract class Apparence
+{
+	protected $taille;
+	protected $poids;
+	protected $peau;
+	protected $cheveux;
+	protected $pilosite;
+	protected $corpulence;
+	
+	public function getTaille()
+	{
+		return $this->taille;
+	}
+	
+	public function getPoids()
+	{
+		return $this->poids;
+	}
+	
+	public function getPeau()
+	{
+	  return $this->peau;
+	}
+	
+	public function getCheveux()
+	{
+	  return $this->cheveux;
+	}
+	
+	public function getPilosite()
+	{
+	  return $this->pilosite;
+	}
+	
+	public function getCorpulence()
+	{
+	  return $this->corpulence;
+	}
+	
+	public function setTaille($taille)
+	{
+		$this->taille = $taille;
+	}
+	
+	public function setPoids($poids)
+	{
+		$this->poids = $poids;
+	}
+}
