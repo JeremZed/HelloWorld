eceabd800bd0c9a0b9104c34c67d2b31846d70bc
JeremZed <j.thulliez@gmail.com>
2015-04-30 13:28:12 +0200

    Update Collection.class.php

--- Core/Collection.class.php
+++ Core/Collection.class.php
@@ -10,4 +10,29 @@ class Collection
     {
         $this->array = $tab;
     }
+    
+    /**
+     * @desc    Retourne la collection
+     * @return  Array
+     */
+    public function getArray()
+    {
+        return $this->array;
+    }
+    
+    /**
+     * @desc    Retourne une occurence du tableau en fonction de sa clé
+     * @return  Si existe alors le contenu Sinon null
+     */
+    public function get($key)
+    {
+        if(isset($this->array[$key]))
+        {
+            return $this->array[$key];
+        }
+        else
+        {
+            return null;
+        }
+    }
 }
