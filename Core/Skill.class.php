85ce24106966adb695b1e85de106f7df800b8277
JeremZed <j.thulliez@gmail.com>
2015-04-30 17:04:37 +0200

    Update Skill.class.php

--- Core/Skill.class.php
+++ Core/Skill.class.php
@@ -14,9 +14,33 @@ abstract class Skill
 		return $this->libelle;
 	}
 	
+	public function getDescription()
+	{
+		return $this->description;
+	}
+	
+	public function getType()
+	{
+		return $this->type;
+	}
+	
 	public function setLibelle($libelle)
 	{
 		$this->libelle = $libelle;
 	}
 	
+	public function setDescription($str)
+	{
+		$this->description = $str;
+	}
+	
+	public function setType($type)
+	{
+		$this->type = $type;
+	}
+	
+	public function setValue($value)
+	{
+		$this->value = $value;
+	}
 }
