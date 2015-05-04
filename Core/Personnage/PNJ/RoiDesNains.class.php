4dd3ebbf58d824479c47247b88461a8c70e34ff0
JeremZed <j.thulliez@gmail.com>
2015-05-04 11:21:17 +0200

    Create RoiDesNains.class.php

---
+++ Core/Personnage/PNJ/RoiDesNains.class.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core\Personnage\PNJ;
+
+use Core\Personnage\PNJ;
+
+class RoiDesNains extends PNJ
+{
+	public function __construct()
+	{
+		$this->setNom('Roi des nains.');
+	}
+}
