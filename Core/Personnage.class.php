fb8f5cbaea265cd3a50f3ab0474d519194461c65
JeremZed <j.thulliez@gmail.com>
2015-05-04 10:49:12 +0200

    Update Personnage.class.php

--- Core/Personnage.class.php
+++ Core/Personnage.class.php
@@ -5,6 +5,18 @@ namespace Core;
 abstract class Personnage
 {
     protected $nom;
-    
+    protected $droitier;
+    protected $gaucher;
+    protected $experience;
+    protected $sexe;
+    protected $apparence;
+    protected $race;
+    protected $classe;
+    protected $equipement;
+    protected $caracteristique;
+    protected $inventaire;
+    protected $banque;
+    protected $metier;
+    protected $recette;
 
 }
