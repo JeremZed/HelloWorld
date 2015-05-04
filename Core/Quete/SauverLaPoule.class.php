9f1a44fbcd2d8d6a38619d02468f12423f1cd3de
JeremZed <j.thulliez@gmail.com>
2015-05-04 16:26:23 +0200

    Create SauverLaPoule.class.php

---
+++ Core/Quete/SauverLaPoule.class.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core\Quete;
+
+use Core\Quete;
+
+class SauverLaPoule extends Quete
+{
+        public function __construc()
+        {
+            $this->setNom('Il faut sauver la poule!!');
+        }
+}
