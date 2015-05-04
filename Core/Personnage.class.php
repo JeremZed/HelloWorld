5d8cc5ddfc98394fefb7bed5b17ad80d1030eea9
JeremZed <j.thulliez@gmail.com>
2015-05-04 10:51:58 +0200

    Update Personnage.class.php

--- Core/Personnage.class.php
+++ Core/Personnage.class.php
@@ -18,5 +18,60 @@ abstract class Personnage
     protected $banque;
     protected $metier;
     protected $recette;
+    
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    
+    public function getDroitier()
+    {
+        return $this->droitier;
+    }
+    
+    public function getGaucher()
+    {
+        return $this->gaucher;
+    }
+    
+    public function getExperience()
+    {
+        return $this->experience;
+    }
+    
+    public function getSexe()
+    {
+        return $this->sexe;
+    }
+    
+    public function getApparence()
+    {
+        return $this->apparence;
+    }
+    
+    public function getRace()
+    {
+        return $this->race;
+    }
+    
+    public function getClasse()
+    {
+        return $this->classe;
+    }
+    
+    public function getEquipement()
+    {
+        return $this->equipement;
+    }
+    
+    public function getCaracteristique()
+    {
+        return $this->caracteristique;
+    }
+    
+    public function getInventaire()
+    {
+        return $this->inventaire;
+    }
 
 }
