079cfe84c6620802bec34464c3c91139ac2df13d
JeremZed <j.thulliez@gmail.com>
2015-05-05 15:52:17 +0200

    Update Route.class.php

--- Engine/Route.class.php
+++ Engine/Route.class.php
@@ -5,7 +5,11 @@ namespace Engine;
 class Route
 {
     protected $controller;
-    protected $action;        
+    protected $action;
+    protected $module;
+
+    protected $instance_controller;
+    
     protected $routes;
     protected $route;
     
@@ -70,6 +74,23 @@ class Route
     }
     
     /**
+     * @desc    Permet de retourner le module associé à la route
+     * @return  string
+     */
+    public function getModule()
+    {
+        return $this->module;
+    }
+    
+    /**
+     * @desc    permet de retourner l'instance du controller de la route
+     */
+    public function getInstanceController()
+    {
+        return $this->instance_controller;
+    }
+    
+    /**
      * @desc    Permet de savoir si la configuration de la route possède un controller
      * @param array $data
      * @throws ErrorException
@@ -82,11 +103,17 @@ class Route
             throw new ErrorException('La configuration de la route <b>'.$this->route.'</b> ne possède pas de controller.');
         }
         
+        if(!file_exists('Application/'.$data['module'].'/Controller/'.$data['controller'].'Controller.class.php'))
+        {
+            throw new ErrorException('Le controller <b>'.$data['controller'].'</b> n\'existe pas dans le module <i>'.$data['module'].'</i>.');
+        }
+        
         return true;
     }
     
     /**
-     * @desc    Permet de savoir si la configuration de la route possède une action
+     * @desc    Permet de savoir si la configuration de la route et le controller possède l'action demandé
+     *          Puis affecte l'instance du controller
      * @param array $data
      * @throws ErrorException
      * @return boolean
@@ -97,16 +124,57 @@ class Route
         {
             throw new ErrorException('La configuration de la route <b>'.$this->route.'</b> ne possède pas d\'action.');
         }
+        
+        if(file_exists('Application/'.$data['module'].'/Controller/'.$data['controller'].'Controller.class.php'))
+        {
+            require_once 'Application/'.$data['module'].'/Controller/'.$data['controller'].'Controller.class.php';
+            
+            $class = $data['controller'].'Controller';
+            $instance = new $class();
+            
+            if(!method_exists($instance, $data['action'].'Action'))
+            {
+                throw new ErrorException('L\'action <b>' . $data['action'] . '</b> n\'existe pas pour le controller <i>'.$data['controller'].'</i>');
+            }
+            
+            $this->instance_controller = $instance;
+        }
     
         return true;
     }
     
     /**
-     * @desc    permet d'hydrater les attributs de la classe
+     * @desc    Permet de savoir si la configuration de la route possède un module
+     * @param array $data
+     * @throws ErrorException
+     * @return boolean
+     */
+    public function existModule(array $data)
+    {
+        if(!isset($data['module']))
+        {
+            throw new ErrorException('La configuration de la route <b>'.$this->route.'</b> ne possède pas de module.');
+        }
+        
+        if(!file_exists('Application/'.$data['module']))
+        {
+            throw new ErrorException('Le module <b>'.$data['module'].'</b> n\'existe pas.');
+        }
+    
+        return true;
+    }
+    
+    /**
+     * @desc    permet d'hydrater les attributs de la classe (controller, action, module)
      * @param array $data
      */
     public function hydrate(array $data)
     {
+        if($this->existModule($data))
+        {
+            $this->module = $data['module'];
+        }
+        
         if($this->existController($data))
         {
             $this->controller = $data['controller'];
