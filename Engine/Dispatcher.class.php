42ac424b9f203b5307d28503fddadee7d70e1501
JeremZed <j.thulliez@gmail.com>
2015-05-05 15:02:06 +0200

    Update Dispatcher.class.php

--- Engine/Dispatcher.class.php
+++ Engine/Dispatcher.class.php
@@ -5,19 +5,85 @@ namespace Engine;
 class Dispatcher
 {
     protected $route;
-    
+    protected $_get;
+    protected $_post;
+            
     public function __construct($route=null)
     {
-        $this->route = $this->setRoute($route);
+        $this->_get = $_GET;
+        $this->setRoute($route);
     }
     
+    /**
+     * @desc    Permet d'affecter la valeur de la route en fonction du $_GET ou via le constructeur
+     * @param unknown $route
+     */
     public function setRoute($route)
-    {
-        $this->route = $route;
+    {            
+        if(isset($route))
+        {
+            $this->route = $route;               
+        }
+        elseif($this->getExist('route')) 
+        {
+            $this->route = $this->get('route');                
+        }
+        else
+        {
+            throw new ErrorException('Aucune route à charger.');
+        }            
     }
     
+    /**
+     * @desc    Permet de retourner la route encours
+     * @return Ambigous <unknown, NULL>
+     */
+    public function getRoute()
+    {
+        return $this->route;
+    }
+    
+    /**
+     * @desc    Permet de retourner la valeur du $_GET en fonction de sa clé, sinon null
+     * @param mixed $key
+     * @return NULL
+     */
+    public function get($key)
+    {
+        if(isset($this->_get[$key]))
+        {
+            return $this->_get[$key];
+        }
+        else 
+        {
+            return null;
+        }
+    }
+    
+    /**
+     * @desc    Permet de savoir si une variable $_GET existe en indiquant son nom
+     * @param mixed $key
+     */
+    public function getExist($key)
+    {
+        return (isset($this->_get[$key]));
+    }
+    
+    /**
+     * @desc    Permet de lancer le traitement correspondant à la route demandée
+     */
     public function execute()
     {
-        
+        $Route = new Route($this->route);            
+                   
+        if($Route->exist())
+        {
+            $controller = $Route->getController();
+            $action = $Route->getAction();                
+        }
+        else
+        {
+            throw new ErrorException('Cette route ' . $this->route . ' n\'existe pas.');
+        }
     }
 }
