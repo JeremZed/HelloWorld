4b6896f5bd1accff65ca0edc1ea55df0f9d7ca3b
JeremZed <j.thulliez@gmail.com>
2015-05-05 13:39:10 +0200

    Update Application.class.php

--- Engine/Application.class.php
+++ Engine/Application.class.php
@@ -4,5 +4,24 @@ namespace Engine;
 
 class Application
 {
-
+    public function __construct()
+    {
+        
+    }
+    
+    /**
+     * @desc    Point d'entré de l'application
+     */
+    public function run()
+    {
+        
+    }
+    
+    /**
+     * @desc    Permet de retourner le visuel de la page demandée
+     */ 
+    public function render()
+    {
+        return '';
+    }
 }
