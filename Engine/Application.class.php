dd2aae05258483364615ef909a4e359ddc146c1f
JeremZed <j.thulliez@gmail.com>
2015-05-05 15:00:49 +0200

    Update Application.class.php

--- Engine/Application.class.php
+++ Engine/Application.class.php
@@ -7,24 +7,16 @@ use Engine\Dispatcher;
 class Application
 {
     public function __construct()
-    {
-        
-    }
+    {}
     
-    /**
-     * @desc    Point d'entré de l'application
-     */
     public function run()
     {
         $Dispatcher = new Dispatcher();
         $Dispatcher->execute();
     }
     
-    /**
-     * @desc    Permet de retourner le visuel de la page demandée
-     */ 
     public function render()
     {
-        return '';
+        return 'good';
     }
 }
