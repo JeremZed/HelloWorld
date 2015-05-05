5aef85fd8c133c9453fa92e5354bb6eeb26a0753
Jeremy <j.thulliez@gmail.com>
2015-05-05 22:08:14 +0200

    up

--- Loader.php
+++ Loader.php
@@ -11,31 +11,6 @@ function my_autoload($class)
 
 spl_autoload_register('my_autoload');
 
-// $view = 'index';
-// if(isset($_GET['view']))
-// {
-// 	$view = htmlspecialchars($_GET['view']);
-// }
-
-// require_once dirname(__FILE__).'/Twig-1.18.1/lib/Twig/Autoloader.php';
-// Twig_Autoloader::register(true);
-
-// $loader = new Twig_Loader_Filesystem('Templates');
-// $twig = new Twig_Environment($loader, array(
-// 		//'cache' => '/path/to/compilation_cache',
-// ));
-
-// try {
-// 	$template = $twig->loadTemplate($view.'.html');
-// 	$render = $template->render(array('the' => 'variables', 'go' => 'here'));
-// }
-// catch (Exception $e)
-// {
-// 	$render = 'Fail';
-// }
-
-// echo $render;
-
 try{
 	
 	$Application = new Application();
