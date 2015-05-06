e379459f0b702c02ef957bb463e854f141c5cc57
JeremZed <j.thulliez@gmail.com>
2015-05-06 15:46:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <!-- Bootstrap -->
     <link href="Public/css/bootstrap.min.css" rel="stylesheet">
     <link href="Public/css/main.css" rel="stylesheet">
+    <link href="Public/css/css.global.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
