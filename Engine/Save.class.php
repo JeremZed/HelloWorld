8c148a742b8c72328d2ac5e4f05eb262874d1e83
JeremZed <j.thulliez@gmail.com>
2015-05-06 16:32:43 +0200

    Update Save.class.php

--- Engine/Save.class.php
+++ Engine/Save.class.php
@@ -4,6 +4,23 @@ namespace Engine;
 
 class Save
 {
-
-
+    protected $save;
+    
+    public function __construct()
+    {
+        if($this->exist())
+        {
+            $this->save = $this->load();
+        }
+    }
+    
+    public function exist()
+    {
+        
+    }
+    
+    public function load()
+    {
+        
+    }
 }
